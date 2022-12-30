6dc86c58aed433057c8e6da79cceed73ce7a8264
Daniel Vinícius <dev.dan.programador@gmail.com>
2022-12-30 10:29:17 +0000

    Teste Value Object

--- src/@seedwork/domain/value-objects/__tests__/unique_entity_id.vo.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/unique_entity_id.vo.spec.ts
@@ -1,6 +1,6 @@
 import { validate as uuidValidate } from "uuid";
 import { InvalidUuidError } from "@seedwork/errors/invalid_uuid.error";
-import { UniqueEntityId } from "./unique_entity_id.vo";
+import { UniqueEntityId } from "../unique_entity_id.vo";
 
 function spyValidateMethod() {
   return jest.spyOn(UniqueEntityId.prototype as any, "validate");
