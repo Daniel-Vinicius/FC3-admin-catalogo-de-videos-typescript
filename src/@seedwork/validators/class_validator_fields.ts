3e7ea1d8be213b4e9f8d772a058ec39c104f75b3
Daniel Vinícius <dev.dan.programador@gmail.com>
2023-01-07 16:12:25 +0000

    validation service with Class Validator

---
+++ src/@seedwork/validators/class_validator_fields.ts
@@ -0,0 +1,28 @@
+import { validateSync } from "class-validator";
+
+import {
+  FieldsErrors,
+  ValidatorFieldsInterface,
+} from "./validator_fields.interface";
+
+class ClassValidatorFields<PropsValidated>
+  implements ValidatorFieldsInterface<PropsValidated>
+{
+  errors: FieldsErrors = null;
+  validatedData: PropsValidated = null;
+
+  validate(data: any): boolean {
+    const classValidatorErrors = validateSync(data);
+    if (classValidatorErrors.length > 0) {
+      this.errors = {};
+      for (const error of classValidatorErrors) {
+        const field = error.property;
+        this.errors[field] = Object.values(error.constraints);
+      }
+    } else {
+      this.validatedData = data;
+    }
+
+    return classValidatorErrors.length < 1;
+  }
+}
