b306f9cf70a357d988e96b0e6ad179e43e2e09fb
Daniel Vinícius <dev.dan.programador@gmail.com>
2023-01-11 06:12:15 +0000

    applyFilter test

--- src/@seedwork/domain/repository/__tests__/in_memory_searchable.repository.spec.ts
+++ src/@seedwork/domain/repository/__tests__/in_memory_searchable.repository.spec.ts
@@ -13,7 +13,7 @@ class StubInMemorySearchableRepository extends InMemorySearchableRepository<Stub
 
   protected async applyFilter(
     items: StubEntity[],
-    filter: string
+    filter: string | null
   ): Promise<StubEntity[]> {
     if (!filter) {
       return items;
@@ -36,7 +36,39 @@ describe("InMemorySearchableRepository Unit Tests", () => {
   let repository: StubInMemorySearchableRepository;
   beforeEach(() => (repository = new StubInMemorySearchableRepository()));
 
-  describe("applyFilter method", () => {});
+  describe("applyFilter method", () => {
+    it("should not filter items when filter param is null", async () => {
+      const items = [new StubEntity({ name: "Shirt", price: 20 })];
+
+      const spyFilterMethod = jest.spyOn(items, "filter");
+      const itemsFiltered = await repository["applyFilter"](items, null);
+
+      expect(spyFilterMethod).not.toHaveBeenCalled();
+      expect(itemsFiltered).toStrictEqual(items);
+    });
+
+    it("should filter using a filter param", async () => {
+      const items = [
+        new StubEntity({ name: "SHIRT", price: 20 }),
+        new StubEntity({ name: "shirt", price: 20 }),
+        new StubEntity({ name: "fake", price: 0 }),
+      ];
+
+      const spyFilterMethod = jest.spyOn(items, "filter");
+      let itemsFiltered = await repository["applyFilter"](items, "SHIRT");
+
+      expect(spyFilterMethod).toHaveBeenCalled();
+      expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+
+      itemsFiltered = await repository["applyFilter"](items, "20");
+      expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+      expect(spyFilterMethod).toHaveBeenCalledTimes(2);
+
+      itemsFiltered = await repository["applyFilter"](items, "no-filter");
+      expect(itemsFiltered.length).toEqual(0);
+      expect(spyFilterMethod).toHaveBeenCalledTimes(3);
+    });
+  });
 
   describe("applySort method", () => {});
 
