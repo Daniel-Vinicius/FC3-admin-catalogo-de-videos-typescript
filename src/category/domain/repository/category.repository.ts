98a1a380a8aabd0302c26dc1e9df672cfef97fa8
Daniel Vinícius <dev.dan.programador@gmail.com>
2023-01-11 07:19:23 +0000

    organizing repository of category using namespaces

--- src/category/domain/repository/category.repository.ts
+++ src/category/domain/repository/category.repository.ts
@@ -1,5 +1,23 @@
-import { SearchableRepositoryInterface } from "@seedwork/domain/repository/repository.contracts";
 import { Category } from "../entities/category";
 
-export interface CategoryRepository
-  extends SearchableRepositoryInterface<Category, any, any> {}
+import {
+  SearchableRepositoryInterface,
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
+} from "@seedwork/domain/repository/repository.contracts";
+
+export namespace CategoryRepository {
+  export type Filter = string;
+
+  export class SearchParams extends DefaultSearchParams<Filter> {}
+
+  export class SearchResult extends DefaultSearchResult<Category, Filter> {}
+
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      Category,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {}
+}
--- src/category/infra/repository/category_in_memory.repository.ts
+++ src/category/infra/repository/category_in_memory.repository.ts
@@ -1,7 +1,26 @@
-import { InMemorySearchableRepository } from "@seedwork/domain/repository/in_memory.repository";
+import { InMemorySearchableRepository } from "@seedwork/domain/repository/in_memory_searchable.repository";
 import { Category } from "category/domain/entities/category";
 import { CategoryRepository } from "category/domain/repository/category.repository";
 
 export class CategoryInMemoryRepository
   extends InMemorySearchableRepository<Category>
-  implements CategoryRepository {}
+  implements CategoryRepository.Repository
+{
+  sortableFields: string[] = ["name"];
+
+  protected async applyFilter(
+    items: Category[],
+    filter: CategoryRepository.Filter
+  ): Promise<Category[]> {
+    if (!filter) {
+      return items;
+    }
+
+    const itemsFiltered = items.filter((item) => {
+      const { name } = item.props;
+      return name.toLowerCase().includes(filter.toLowerCase());
+    });
+
+    return itemsFiltered;
+  }
+}
