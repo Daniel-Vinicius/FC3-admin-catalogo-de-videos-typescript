3daf786f6f982a6fe984e216f870e6d1bb4eb748
Daniel Vinícius <dev.dan.programador@gmail.com>
2023-01-12 01:38:37 +0000

    UpdateCategoryUseCase

---
+++ src/category/application/usecases/__tests__/update_category.usecase.spec.ts
@@ -0,0 +1,85 @@
+import { CategoryInMemoryRepository } from "category/infra/repository/category_in_memory.repository";
+import { UpdateCategoryUseCase } from "category/application/usecases/update_category.usecase";
+import { NotFoundError } from "@seedwork/domain/errors/not_found.error";
+import { Category } from "category/domain/entities/category";
+
+describe("UpdateCategoryUseCase Unit Tests", () => {
+  let categoryRepository: CategoryInMemoryRepository;
+  let useCase: UpdateCategoryUseCase;
+
+  beforeEach(() => {
+    categoryRepository = new CategoryInMemoryRepository();
+    useCase = new UpdateCategoryUseCase(categoryRepository);
+  });
+
+  it("should throws error when category not found", async () => {
+    const id = "fake-id";
+
+    expect(
+      async () => await useCase.execute({ id, name: "updated" })
+    ).rejects.toThrowError(
+      new NotFoundError(`Entity Not Found using ID ${id}`)
+    );
+  });
+
+  it("should update a category", async () => {
+    const category = new Category({ name: "Horror" });
+    categoryRepository.items = [category];
+
+    const arrange = [
+      {
+        input: {
+          id: category.id,
+          name: "Comedy",
+          description: "Category updated",
+          is_active: false,
+        },
+        outputExpected: {
+          id: category.id,
+          name: "Comedy",
+          description: "Category updated",
+          is_active: false,
+          created_at: category.created_at,
+        },
+      },
+      {
+        input: {
+          id: category.id,
+          name: "Comedy",
+          is_active: true,
+        },
+        outputExpected: {
+          id: category.id,
+          name: "Comedy",
+          description: null as any,
+          is_active: true,
+          created_at: category.created_at,
+        },
+      },
+      {
+        input: {
+          id: category.id,
+          name: "Comedy Updated",
+          description: "Some Description",
+        },
+        outputExpected: {
+          id: category.id,
+          name: "Comedy Updated",
+          description: "Some Description",
+          is_active: true,
+          created_at: category.created_at,
+        },
+      },
+    ];
+
+    for (const variation of arrange) {
+      const spyUpdate = jest.spyOn(categoryRepository, "update");
+      const { input, outputExpected } = variation;
+
+      const output = await useCase.execute(input);
+
+      expect(output).toEqual(outputExpected);
+      expect(spyUpdate).toHaveBeenCalledWith(category);
+    }
+  });
+});
---
+++ src/category/application/usecases/update_category.usecase.ts
@@ -0,0 +1,36 @@
+import { UseCase } from "@seedwork/application/usecase";
+import { CategoryRepository } from "category/domain/repository/category.repository";
+import {
+  CategoryOutputDTO,
+  CategoryOutputMapper,
+} from "../dtos/category_output.dto";
+
+export type Input = {
+  id: string;
+  name: string;
+  description?: string;
+  is_active?: boolean;
+};
+
+export type Output = CategoryOutputDTO;
+
+export class UpdateCategoryUseCase implements UseCase<Input, Output> {
+  constructor(private categoryRepository: CategoryRepository.Repository) {}
+
+  async execute({ id, name, description, is_active }: Input): Promise<Output> {
+    const entity = await this.categoryRepository.findById(id);
+    entity.update(name, description);
+
+    if (is_active === true) {
+      entity.activate();
+    }
+
+    if (is_active === false) {
+      entity.deactivate();
+    }
+
+    await this.categoryRepository.update(entity);
+
+    return CategoryOutputMapper.toOutputDTO(entity);
+  }
+}
