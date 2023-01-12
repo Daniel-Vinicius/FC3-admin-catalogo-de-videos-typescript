07f8012d27b88d60e19e8c4c0b89c5a2a7eb6739
Daniel Vinícius <dev.dan.programador@gmail.com>
2023-01-12 04:02:09 +0000

    DeleteCategoryUseCase

---
+++ src/category/application/usecases/__tests__/delete_category.usecase.spec.ts
@@ -0,0 +1,36 @@
+import { CategoryInMemoryRepository } from "category/infra/repository/category_in_memory.repository";
+import { DeleteCategoryUseCase } from "category/application/usecases/delete_category.usecase";
+import { NotFoundError } from "@seedwork/domain/errors/not_found.error";
+import { Category } from "category/domain/entities/category";
+
+describe("DeleteCategoryUseCase Unit Tests", () => {
+  let categoryRepository: CategoryInMemoryRepository;
+  let useCase: DeleteCategoryUseCase;
+
+  beforeEach(() => {
+    categoryRepository = new CategoryInMemoryRepository();
+    useCase = new DeleteCategoryUseCase(categoryRepository);
+  });
+
+  it("should throws error when category not found", async () => {
+    const spyDelete = jest.spyOn(categoryRepository, "delete");
+    const id = "fake-id";
+
+    expect(async () => await useCase.execute({ id })).rejects.toThrowError(
+      new NotFoundError(`Entity Not Found using ID ${id}`)
+    );
+
+    expect(spyDelete).toHaveBeenCalledTimes(1);
+  });
+
+  it("should deletes a category", async () => {
+    const spyDelete = jest.spyOn(categoryRepository, "delete");
+    const category = new Category({ name: "Movie" });
+    categoryRepository.items = [category];
+
+    await useCase.execute({ id: category.id });
+
+    expect(spyDelete).toHaveBeenCalledTimes(1);
+    expect(categoryRepository.items).toHaveLength(0);
+  });
+});
---
+++ src/category/application/usecases/delete_category.usecase.ts
@@ -0,0 +1,16 @@
+import { UseCase } from "@seedwork/application/usecase";
+import { CategoryRepository } from "category/domain/repository/category.repository";
+
+export type Input = {
+  id: string;
+};
+
+export type Output = void;
+
+export class DeleteCategoryUseCase implements UseCase<Input, Output> {
+  constructor(private categoryRepository: CategoryRepository.Repository) {}
+
+  async execute({ id }: Input): Promise<Output> {
+    await this.categoryRepository.delete(id);
+  }
+}
